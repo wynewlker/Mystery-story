b9fe52aa672ef4ad9d1884f82c0e1329961f28ca
wynewlker <148379295+wynewlker@users.noreply.github.com>
2023-10-19 16:48:46 -0400

    halloween text image added?

--- Challenge 2 Story Project/Chapter 1/Chapter1.swift
+++ Challenge 2 Story Project/Chapter 1/Chapter1.swift
@@ -5,8 +5,23 @@
 
 import Foundation
 
+//added halloween text
+
 func chapterOne() {
-    print("Once upon a time there was")
-    //added text for the beginning
-    // Your portion of the story goes here
+    
+print("""
+                   ___  ___
+         \\  \\  /`\\ \\  \\ \\  \\ \\  \\
+          \\__\\ \\__\\ \\__' \\__' \\__\\
+           \\  \\ \\  \\ \\    \\     \\
+            \\  \\ \\  \\ \\    \\     \\
+                    ___         ___  ___
+\\  \\  /`\\ \\    \\    \\  \\ \\    \\ \\    \\    \\.  \\
+ \\__\\ \\__\\ \\    \\    \\  \\ \\  . \\ \\__  \\__  \\`\\ \\
+  \\  \\ \\  \\ \\    \\    \\  \\ \\ |`\\\\ \\    \\    \\ `\\\\
+   \\  \\ \\  \\ \\___ \\___ \\__\\ \\|  `\\ \\___ \\___ \\  `\\
+
+""")
+    print("Once upon a time there was a young woman")
+  
 }
