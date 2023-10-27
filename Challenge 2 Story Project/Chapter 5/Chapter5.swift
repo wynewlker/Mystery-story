0e566b0d11342b45ea31048b50000a722cbc368b
Olga <luna33040@gmail.com>
2023-10-27 15:26:21 -0400

    resolved merge conflict

--- Challenge 2 Story Project/Chapter 5/Chapter5.swift
+++ Challenge 2 Story Project/Chapter 5/Chapter5.swift
@@ -5,12 +5,11 @@
 
 import Foundation
 
- lawayne-chapter1
 func chapterFive() {
     
-
-func chapterFive() {print("They all lived happily ever after")
-   //test of text
- main
-    // Your portion of the story goes here
+    
+    func chapterFive() {print("They all lived happily ever after")
+        //test of text
+        // Your portion of the story goes here
+    }
 }
