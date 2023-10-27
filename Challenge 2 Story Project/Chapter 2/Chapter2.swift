5446e7722b525b7a5b27dc363838b2b2bda91459
stephanie Logan <sl3753@yahoo.com>
2023-10-27 15:28:07 -0400

    chapter 2 print statements

--- Challenge 2 Story Project/Chapter 2/Chapter2.swift
+++ Challenge 2 Story Project/Chapter 2/Chapter2.swift
@@ -1,11 +1,15 @@
-//
-//  Chapter2.swift
-//  CLIStoryTemplate
-//
-
-import Foundation
+import SwiftUI
 
 
-func chapterTwo() { print("This is Stephanie chapter")
-    // Your portion of the story goes here
+func chapterTwo() {
+    print("This is Stephanie's Chapter")
+    print("This Chapter is called The Corner Store")
+    print("It was a hot burning day in Mansfield Ohio")
+    print("Two young girls name Tonya and Beth asked their mother could they go to the store")
+    print("The mother said")
+    func sayYesGirls() -> String {
+        return "yes, girls"
+    }
+
+    
 }
