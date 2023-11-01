4d505f63585fe9b0f0fe80f6b8c52a98b93ef481
Calil Hall <Calilihall@gmail.com>
2023-11-01 16:20:47 -0400

    Updates 5

--- Challenge 2 Story Project/Chapter 5/Chapter5.swift
+++ Challenge 2 Story Project/Chapter 5/Chapter5.swift
@@ -1,15 +1,55 @@
-//
-//  Chapter5.swift
-//  CLIStoryTemplate
-//
 
 import Foundation
 
 func chapterFive() {
     
+var Person1 = "Calil"
+var Person2 = "Marshall"
+var Person3 = "Slim"
+var yOrN = yesOrNo(userAnswer: true)
+var Answer: String = yesOrNo(userAnswer: Bool.random())
+
+func Intro() -> String {
+return "You Want to hear a story? \n Yes YOU \n Trust me, it will be a good one. \n Do you want to hear it or not?"}
+
+func yesOrNo(userAnswer: Bool) -> String {
+    if userAnswer == true {
+        return "Yes?"
+    } else {
+        return "No?"
+}}
+
+func YorNResponse(yOrN: String) -> String {
+    if  yOrN == "Yes?" {
+        return "Well lets get started"
+    } else {
+        return "Well..... Im telling you anyway"}
+}
+func setting() -> String {
+    return "\n      In a small town nestled amidst rolling hills and dense forests, a sudden scream shattered the tranquil night. \(Person1), a seasoned detective known for his sharp instincts, rushed to the scene, his mind already racing with potential leads. The dim streetlights cast eerie shadows over the crime scene, intensifying the grim atmosphere. Examining the brutal environment, \(Person1)'s sturn look conveyed his determination to find answers."
+}
+
+func Scene1() -> String {
+    return "  \n     The victim's lifeless body lay sprawled on the ground, surrounded by a maze of clues, each one leading to more questions. As he meticulously collected evidence, his mind wandered to \(Person3), the brilliant neuroscientist known for his ability to unravel the most complex puzzles. \(Person1) knew he would need \(Person3)'s keen insights to crack this case. Meanwhile, \(Person2), a police chief, paced back and forth, haunted by the images from the crime scene. His usual calm demeanor was overshadowed by the gravity of the situation. With every step, he felt the weight of the impending investigation, a puzzle that seemed to extend far beyond the confines of the small town."
+}
+func Scene2() -> String {
+    return "\n     As the sun rose over the town,\(Person1) \(Person2) and \(Person3) convened at the town hall. The trio exchanged somber glances, each acknowledging the weight of the situation. They delved into the intricate web of relationships and potential motives among the townspeople, suspecting that the answers lay hidden in the town's closely guarded secrets. Their investigation led them to a local café, where they discreetly observed the residents, searching for any hint of a motive or a connection to the crime. The bustling café seemed like a microcosm of the town, teeming with whispered conversations and guarded glances. Their pursuit of the truth eventually led them to a dark alleyway, where they confronted a formidable adversary, someone determined to protect the town's secrets at any cost. \n \n      \(Person1), fueled by a sense of justice, faced the adversary with unwavering determination, his training and intuition guiding him through the dangerous confrontation.You've been pulling the strings all along? \(Person1)'s voice resonated through the alleyway, laden with a mix of anger and frustration. How many more have to die for your secrets? As the conflict reached its climax, \(Person1)'s relentless pursuit of the truth prevailed, unmasking the mastermind behind the murder mystery that had shaken the town to its core. The town, once cloaked in a veil of tranquility, slowly began to rebuild, its secrets exposed to the light, allowing its residents to confront their shared history and forge a new path toward healing and redemption."
     
-    func chapterFive() {print("They all lived happily ever after")
-        //test of text
-        // Your portion of the story goes here
-    }
+}
+    print(Intro())
+    print(Answer)
+    print(YorNResponse(yOrN: Answer))
+    print(setting())
+    print(Scene1())
+    print(Scene2())
+    
+
+
+
+
+
+    
+    
+    
+    
 }
